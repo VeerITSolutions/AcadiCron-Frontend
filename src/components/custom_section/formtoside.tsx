85053a8c47f58c4148a2f0e45b854775a31a2047
pranay-lamse <pranay.l@veerit.com>
2024-08-20 11:20:57 +0530

    added

---
+++ src/components/custom_section/formtoside.tsx
@@ -0,0 +1,50 @@
+// app/components/formtoside.tsx
+"use client"; // This marks the component as a Client Component
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const formtoside = () => {
+  const router = useRouter();
+
+  return (
+    <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
+      <div className="flex flex-col gap-9">
+        <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+          <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
+            <h3 className="font-medium text-black dark:text-white">
+              Add Student House
+            </h3>
+          </div>
+          <div className="flex flex-col gap-5.5 p-6.5">
+            <div>
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                Admission No *
+              </label>
+              <input
+                type="text"
+                placeholder=""
+                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-5.5 p-6.5">
+            <div>
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                Roll Number
+              </label>
+              <input
+                type="text"
+                placeholder=""
+                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default formtoside;
