b0272e4a09522978e0b6c7273ee4631a7254bffd
pranay-lamse <pranay.l@veerit.com>
2024-08-21 15:24:14 +0530

    added

--- src/components/Tables/TableFour.tsx
+++ src/components/Tables/TableFour.tsx
@@ -47,7 +47,8 @@ const brandData: BRAND[] = [
 
 const TableFour: React.FC = () => {
   return (
-    <div className="col-span-12 xl:col-span-7">
+    <></>
+    /*   <div className="col-span-12 xl:col-span-7">
       <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
         <div className="mb-6 flex justify-between">
           <div>
@@ -118,7 +119,7 @@ const TableFour: React.FC = () => {
           ))}
         </div>
       </div>
-    </div>
+    </div> */
   );
 };
 
