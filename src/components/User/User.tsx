f6b87dbc79af9ee286e2e5a05a7e7ebd27f2fd51
nilesh.b@veerit.com <rahul.u@veerit.com>
2024-08-26 16:11:51 +0530

    form changes

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -19,7 +19,7 @@ const User = () => {
     <>
       {/* <Breadcrumb pageName="FormElements" /> */}
 
-      <div className="grid grid-cols-1 gap-9 sm:grid-cols-2">
+      <div className="student_admission_form grid ">
         <div className="flex flex-col gap-9">
           {/* <!-- Input Fields --> */}
           <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
@@ -28,10 +28,10 @@ const User = () => {
                 Student Admission
               </h3>
             </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
+            <div className="grid gap-5.5 p-6.5 sm:grid-cols-4">
+              <div className="field">
                 <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Admission No *
+                  Admission No <span className="required">*</span>
                 </label>
                 <input
                   type="text"
@@ -39,10 +39,7 @@ const User = () => {
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 />
               </div>
-            </div>
-
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
+              <div className="field">
                 <label className="mb-3 block text-sm font-medium text-black dark:text-white">
                   Roll Number
                 </label>
@@ -52,240 +49,30 @@ const User = () => {
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 />
               </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
+              <div className="field">
                 <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Class *
+                  Class <span className="required">*</span>
                 </label>
-                <input
-                  type="text"
-                  placeholder=""
+                <select
+                  id="class_id"
+                  name="class_id"
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Section *
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  First Name *
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Gender *
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Date of Birth *
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Category
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Religion
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Caste
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Mobile Number
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Email
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Admission Date
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Blood Group
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Student House
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Height
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Weight
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  As on Date
-                </label>
-                <input
-                  type="date"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Place of Birth
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-5.5 p-6.5">
-              <div>
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Nationality
-                </label>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
+                >
+                  <option value="">Select</option>
+                  <option value="1">Class 1</option>
+                  <option value="2">Class 2</option>
+                  <option value="3">Class 3</option>
+                  <option value="4">Class 4</option>
+                  <option value="5">Class 5</option>
+                  <option value="6">Class 6</option>
+                  <option value="7">Class 7</option>
+                  <option value="8">Class 8</option>
+                  <option value="9">Class 9</option>
+                  <option value="10">Class 10</option>
+                  <option value="11">Nursery</option>
+                  <option value="12">K.G.-I</option>
+                  <option value="13">K.G. - II</option>
+                </select>
               </div>
             </div>
           </div>
