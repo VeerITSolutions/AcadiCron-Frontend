5394cdc7494dea5996a37712b2bfad4c2776218c
pranay-lamse <pranay.l@veerit.com>
2024-09-04 15:52:20 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-
+import '../css/globals.css';
 export const metadata: Metadata = {
   title: "Era International School",
   description: "Era International School",
