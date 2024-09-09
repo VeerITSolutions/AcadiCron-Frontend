1f499ebfa192ef4c5cb97a96690ac81709632269
pranay-lamse <pranay.l@veerit.com>
2024-09-09 18:30:15 +0530

    added

---
+++ src/services/timeTableService.ts
@@ -0,0 +1,25 @@
+import apiClient from "./apiClient";
+
+export const fetchtimeTableData = async (page: number, perPage: number) => {
+  const response = await apiClient.get(`/timetable`, {
+    params: {
+      page,
+      perPage,
+    },
+  });
+  return response.data;
+};
+
+/*
+// Delete a student category by ID
+export const deleteStudent = async (id: number) => {
+  const response = await apiClient.delete(`/admin/dtstudentlist/${id}`);
+  return response.data;
+};
+
+// Edit a student category by ID
+export const editStudent = async (id: number, category: string) => {
+  const data = { category }; // Create an object with the name field
+  const response = await apiClient.put(`/admin/dtstudentlist/${id}`, data);
+  return response.data;
+}; */
