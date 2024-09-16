64e125a3b80af2b19cd9a7dca9d351df1e4a591a
pranay-lamse <pranay.l@veerit.com>
2024-09-16 18:28:17 +0530

    added

--- src/components/User/NoticeForm.tsx
+++ src/components/User/NoticeForm.tsx
@@ -1,28 +1,29 @@
 "use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import CheckboxFive from "@/components/Checkboxes/CheckboxFive";
-import CheckboxFour from "@/components/Checkboxes/CheckboxFour";
-import CheckboxOne from "@/components/Checkboxes/CheckboxOne";
-import CheckboxThree from "@/components/Checkboxes/CheckboxThree";
-import CheckboxTwo from "@/components/Checkboxes/CheckboxTwo";
-import SwitcherFour from "@/components/Switchers/SwitcherFour";
-import SwitcherOne from "@/components/Switchers/SwitcherOne";
-import SwitcherThree from "@/components/Switchers/SwitcherThree";
-import SwitcherTwo from "@/components/Switchers/SwitcherTwo";
-import DatePickerTwo from "@/components/FormElements/DatePicker/DatePickerTwo";
-import DatePickerOne from "@/components/FormElements/DatePicker/DatePickerOne";
-import MultiSelect from "@/components/FormElements/MultiSelect";
-import SelectGroupTwo from "@/components/SelectGroup/SelectGroupTwo";
-import styles from "./User.module.css";
-
 import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
 import { useState } from "react";
+import styles from "./User.module.css";
+import {
+  createNotification,
+  editNotificationData,
+} from "@/services/notificationService";
+import "react-quill/dist/quill.snow.css";
+import toast from "react-hot-toast";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 const NoticeForm = () => {
   const [value, setValue] = useState("");
+  const [formData, setFormData] = useState({
+    department_name: "",
+    publishon: "",
+    message_to: [],
+    classes: [],
+    file: null,
+  });
+
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editCategoryId, setEditCategoryId] = useState<number | null>(null);
 
   const modules = {
     toolbar: [
@@ -31,6 +32,88 @@ const NoticeForm = () => {
       [{ list: "ordered" }, { list: "bullet" }],
       ["link", "image"],
     ],
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
+    if (type === "checkbox") {
+      // Handle checkboxes
+      const arrayName =
+        name === "message_to" || name.startsWith("class") ? name : "";
+      setFormData((prevState) => ({
+        ...prevState,
+        [arrayName]: checked
+          ? [...(prevState[arrayName] || []), value]
+          : prevState[arrayName].filter((item: string) => item !== value),
+      }));
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFormData({ ...formData, file: e.target.files[0] });
+    }
+  };
+
+  const handleEdit = (id: number, department_name_value: string) => {
+    setIsEditing(true);
+    setEditCategoryId(id);
+    setFormData({
+      ...formData,
+      department_name: department_name_value,
+    });
+  };
+
+  const handleSubmit = async () => {
+    const formDataToSend = new FormData();
+    formDataToSend.append("department_name", formData.department_name);
+    formDataToSend.append("message", value);
+    formDataToSend.append("publishon", formData.publishon);
+    formDataToSend.append("message_to", JSON.stringify(formData.message_to));
+    formDataToSend.append("classes", JSON.stringify(formData.classes));
+
+    if (formData.file) {
+      formDataToSend.append("file", formData.file);
+    }
+
+    try {
+      if (isEditing && editCategoryId !== null) {
+        const result = await editNotificationData(
+          editCategoryId,
+          formDataToSend,
+        );
+        if (result.success) {
+          toast.success("Department updated successfully");
+        } else {
+          toast.error("Failed to update Department");
+        }
+      } else {
+        const result = await createNotification(formDataToSend);
+        if (result.success) {
+          toast.success("Department saved successfully");
+        } else {
+          toast.error("Failed to save Department");
+        }
+      }
+
+      // Reset form and state
+      setFormData({
+        department_name: "",
+        publishon: "",
+        message_to: [],
+        classes: [],
+        file: null,
+      });
+      setValue("");
+      setIsEditing(false);
+      setEditCategoryId(null);
+      fetchData(); // Refresh data after submit
+    } catch (error) {
+      toast.error("An error occurred");
+      console.error("An error occurred", error);
+    }
   };
 
   return (
@@ -52,7 +135,9 @@ const NoticeForm = () => {
                 </label>
                 <input
                   type="text"
-                  placeholder=""
+                  name="department_name"
+                  value={formData.department_name}
+                  onChange={handleInputChange}
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 />
               </div>
@@ -64,10 +149,10 @@ const NoticeForm = () => {
                 <input
                   id="publishon"
                   name="publishon"
-                  placeholder=""
+                  value={formData.publishon}
+                  onChange={handleInputChange}
                   type="text"
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                  value="23-08-2024"
+                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                 />
               </div>
 
@@ -87,90 +172,86 @@ const NoticeForm = () => {
               <div className="grid gap-6">
                 <div className="field">
                   <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                    Message To<span className="required">*</span>
-                    &nbsp;&nbsp;&nbsp;
+                    Message To <span className="required">*</span>
                   </label>
-                  <label className="radio-inline mb-3 block text-sm font-medium text-black dark:text-white">
+                  <label className="radio-inline">
                     <input
-                      className="User_radio__Zd0k2"
                       type="checkbox"
                       value="student"
-                      name="student"
+                      name="message_to"
+                      onChange={handleInputChange}
                     />{" "}
                     Student
                   </label>
-                  <label className="radio-inline mb-3 block text-sm font-medium text-black dark:text-white">
+                  <label className="radio-inline">
                     <input
-                      className="User_radio__Zd0k2"
                       type="checkbox"
                       value="parent"
-                      name="parent"
+                      name="message_to"
+                      onChange={handleInputChange}
                     />{" "}
                     Parent
                   </label>
-                  <label className="radio-inline mb-3 block text-sm font-medium text-black dark:text-white">
+                  <label className="radio-inline">
                     <input
-                      className="User_radio__Zd0k2"
                       type="checkbox"
                       value="admin"
-                      name="admin"
+                      name="message_to"
+                      onChange={handleInputChange}
                     />{" "}
                     Admin
                   </label>
-                  <label className="radio-inline mb-3 block text-sm font-medium text-black dark:text-white">
+                  <label className="radio-inline">
                     <input
-                      className="User_radio__Zd0k2"
                       type="checkbox"
                       value="teacher"
-                      name="teacher"
+                      name="message_to"
+                      onChange={handleInputChange}
                     />{" "}
                     Teacher
                   </label>
-                  <span className="text-danger"></span>
                 </div>
 
                 <div className="field">
                   <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                    Multiple Select<span className="required">*</span>
-                    &nbsp;&nbsp;&nbsp;
+                    Multiple Select <span className="required">*</span>
                   </label>
-                  <label className="radio-inline mb-3 block text-sm font-medium text-black dark:text-white">
+                  <label className="radio-inline">
                     <input
-                      className="User_radio__Zd0k2"
                       type="checkbox"
                       value="class1"
-                      name="class1"
+                      name="classes"
+                      onChange={handleInputChange}
                     />{" "}
                     Class 1 to 2
                   </label>
-                  <label className="radio-inline mb-3 block text-sm font-medium text-black dark:text-white">
+                  <label className="radio-inline">
                     <input
-                      className="User_radio__Zd0k2"
                       type="checkbox"
                       value="class2"
-                      name="class2"
+                      name="classes"
+                      onChange={handleInputChange}
                     />{" "}
                     Class 3 to 4
                   </label>
-                  <label className="radio-inline mb-3 block text-sm font-medium text-black dark:text-white">
+                  <label className="radio-inline">
                     <input
-                      className="User_radio__Zd0k2"
                       type="checkbox"
                       value="class3"
-                      name="class3"
+                      name="classes"
+                      onChange={handleInputChange}
                     />{" "}
                     Class 5 to 7
                   </label>
-                  <label className="radio-inline mb-3 block text-sm font-medium text-black dark:text-white">
+                  <label className="radio-inline">
                     <input
-                      className="User_radio__Zd0k2"
                       type="checkbox"
                       value="class4"
-                      name="class4"
+                      name="classes"
+                      onChange={handleInputChange}
                     />{" "}
                     Class 8 to 9
                   </label>
-                  <span className="text-danger"></span>
                 </div>
               </div>
 
@@ -182,16 +263,18 @@ const NoticeForm = () => {
                   className={`form-control mt-2 w-full ${styles["f-13"]}`}
                   type="file"
                   name="file"
-                  id="file"
+                  onChange={handleFileChange}
                 />
               </div>
             </div>
-          </div>
-
-          <div className="flex">
-            <button className="flex items-center gap-2 rounded bg-primary px-4.5 py-2 font-medium text-white hover:bg-opacity-80">
-              Save
-            </button>
+            <div className="px-6.5 py-4">
+              <button
+                onClick={handleSubmit}
+                className="flex w-full justify-center rounded bg-primary p-2 font-semibold text-gray"
+              >
+                Save
+              </button>
+            </div>
           </div>
         </div>
       </div>
