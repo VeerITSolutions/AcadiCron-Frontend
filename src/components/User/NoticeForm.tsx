0f4d6e1647950137e7cb11e154797f5260c21075
pranay-lamse <pranay.l@veerit.com>
2024-09-16 16:37:17 +0530

    added

--- src/components/User/NoticeForm.tsx
+++ src/components/User/NoticeForm.tsx
@@ -16,10 +16,10 @@ import SelectGroupTwo from "@/components/SelectGroup/SelectGroupTwo";
 import styles from "./User.module.css";
 
 import dynamic from "next/dynamic";
-/* import 'react-quill/dist/quill.snow.css'; */
+import "react-quill/dist/quill.snow.css";
 import { useState } from "react";
 
-/* const ReactQuill = dynamic(() => import("react-quill"), { ssr: false }); */
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 const NoticeForm = () => {
   const [value, setValue] = useState("");
