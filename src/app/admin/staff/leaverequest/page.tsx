a1a78ee01bfea90bd5c05c08f3e8d4321af1032a
pranay-lamse <pranay.l@veerit.com>
2024-09-16 16:42:21 +0530

    added

--- src/app/admin/staff/leaverequest/page.tsx
+++ src/app/admin/staff/leaverequest/page.tsx
@@ -4,7 +4,14 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import MUIDataTable from "mui-datatables";
-import { fetchLeaveData, createLeave, deleteLeaveData, editLeaveData } from "@/services/leaveService";
+import {
+  fetchLeaveData,
+  createLeave,
+  deleteLeaveData,
+  editLeaveData,
+} from "@/services/leaveService";
+
+import { fetchLeaveTypeData } from "@/services/leaveTypeService";
 import styles from "./StudentDetails.module.css"; // Import CSS module
 import Loader from "@/components/common/Loader";
 import {
@@ -34,19 +41,18 @@ const columns = [
   "Action",
 ];
 
-
 const options = {
   filterType: false,
   serverSide: true,
   responsive: "standard",
- 
+
   selectableRows: "none", // Disable row selection
   filter: false, // Disable filter,
   viewColumns: false, // Disable view columns button
-
 };
 const StudentDetails = () => {
   const [data, setData] = useState<Array<Array<string>>>([]);
+  const [dataleavetype, setLeaveTypeData] = useState<Array<Array<string>>>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [page, setPage] = useState(0);
@@ -65,18 +71,13 @@ const StudentDetails = () => {
 
   const formatStudentData = (students: any[]) => {
     return students.map((student: any) => [
-     
-     
       student.name || "N/A",
       student.leave_type_id || "N/A",
-      student.leave_from +'-'+ student.leave_to  || "N/A",
+      student.leave_from + "-" + student.leave_to || "N/A",
       student.leave_days || "N/A",
       student.date || "N/A",
       student.status || "N/A",
-     "ACTION"
-      
-
-
+      "ACTION",
     ]);
   };
 
@@ -102,6 +103,20 @@ const StudentDetails = () => {
     } catch (error: any) {
       setError(error.message);
       setLoading(false);
+    }
+
+    try {
+      const result = await fetchLeaveTypeData(
+        currentPage + 1,
+        rowsPerPage,
+        selectedClass,
+        selectedSection,
+        keyword,
+      );
+
+      setLeaveTypeData(result.data);
+    } catch (error: any) {
+      setError(error.message);
     }
   };
   const handleDelete = async (id: number) => {
@@ -162,48 +177,52 @@ const StudentDetails = () => {
 
   return (
     <DefaultLayout>
-     <div className="MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation4 tss-11quiee-MUIDataTable-paper tss-1x5mjc5-MUIDataTable-root rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark StudentDetails_miui-box-shadow__1DvBS css-11mde6h-MuiPaper-root">
-  <div className="text-right sm:w-3/12 xl:w-2/12 pl-4 mb-4 pt-4" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-  <Button 
-  variant="contained" 
-  className="inline-flex rounded bg-white text-white px-3 py-1 font-medium hover:bg-black hover:text-black sm:px-6 sm:py-2.5" 
-  onClick={handleClickOpen}
->
-  Apply Leave
-</Button>
+      <div className="MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation4 tss-11quiee-MUIDataTable-paper tss-1x5mjc5-MUIDataTable-root StudentDetails_miui-box-shadow__1DvBS css-11mde6h-MuiPaper-root rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark">
+        <div
+          className="mb-4 pl-4 pt-4 text-right sm:w-3/12 xl:w-2/12"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            variant="contained"
+            className="inline-flex rounded bg-white px-3 py-1 font-medium text-white hover:bg-black hover:text-black sm:px-6 sm:py-2.5"
+            onClick={handleClickOpen}
+          >
+            Apply Leave
+          </Button>
+        </div>
 
-
-  </div>
-
-
-
-      <MUIDataTable
-        title={"Leaves"} 
-        data={data}
-        className={`rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark ${styles["miui-box-shadow"]}`}
-        columns={columns}
-        options={{
-          ...options,
-          count: totalCount,
-          page: page,
-          rowsPerPage: rowsPerPage,
-          onChangePage: handlePageChange,
-          onChangeRowsPerPage: handleRowsPerPageChange,
-        }}
-      />
-       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle> <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
+        <MUIDataTable
+          title={"Leaves"}
+          data={data}
+          className={`rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark ${styles["miui-box-shadow"]}`}
+          columns={columns}
+          options={{
+            ...options,
+            count: totalCount,
+            page: page,
+            rowsPerPage: rowsPerPage,
+            onChangePage: handlePageChange,
+            onChangeRowsPerPage: handleRowsPerPageChange,
+          }}
+        />
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>
+            {" "}
+            <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
               <h3 className="font-medium text-black dark:text-white">
                 Student Details
               </h3>
             </div>
-            </DialogTitle>
-        <DialogContent>
+          </DialogTitle>
+          <DialogContent>
             <div className="grid gap-5.5 p-6.5 sm:grid-cols-2">
-
               <div className="field">
                 <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                Apply Date
+                  Apply Date
                 </label>
                 <input
                   id="publishon"
@@ -215,14 +234,26 @@ const StudentDetails = () => {
                 />
               </div>
               <div className="field">
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">Available Leave <span className="required">*</span></label>
-                <select id="available_Leave" name="available_Leave" className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary">
-                <option value="select1">Select</option>
-                  </select></div>
+                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                  Available Leave <span className="required">*</span>
+                </label>
+                <select
+                  id="available_Leave"
+                  name="available_Leave"
+                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                >
+                  <option value="select1">Select</option>
+                  {dataleavetype.map((sec) => (
+                    <option key={sec.id} value={sec.id}>
+                      {sec.type}
+                    </option>
+                  ))}
+                </select>
+              </div>
 
               <div className="field">
                 <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                Leave From Date <span className="required">*</span>
+                  Leave From Date <span className="required">*</span>
                 </label>
                 <input
                   id="leavefromdate"
@@ -230,12 +261,11 @@ const StudentDetails = () => {
                   placeholder=""
                   type="text"
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                 
                 />
               </div>
               <div className="field">
                 <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                Leave To Date <span className="required">*</span>
+                  Leave To Date <span className="required">*</span>
                 </label>
                 <input
                   id="leavetodate"
@@ -243,12 +273,11 @@ const StudentDetails = () => {
                   placeholder=""
                   type="text"
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-              
                 />
               </div>
               <div className="field">
                 <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                Reason
+                  Reason
                 </label>
                 <input
                   id="reason"
@@ -256,12 +285,11 @@ const StudentDetails = () => {
                   placeholder=""
                   type="text"
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-          
                 />
               </div>
               <div className="field">
                 <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                Attach Document
+                  Attach Document
                 </label>
                 <input
                   className={`form-control mt-2 w-full ${styles["f-13"]}`}
@@ -270,23 +298,29 @@ const StudentDetails = () => {
                   id="file"
                 />
               </div>
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <div className="flex">
+              <button
+                onClick={handleClose}
+                className="flex items-center gap-2 rounded bg-primary px-4.5 py-2 font-medium text-white hover:bg-opacity-80"
+              >
+                Cancel
+              </button>
+            </div>
+
+            <div className="flex">
+              <button
+                onClick={handleClose}
+                className="flex items-center gap-2 rounded bg-primary px-4.5 py-2 font-medium text-white hover:bg-opacity-80"
+              >
+                Save
+              </button>
+            </div>
+          </DialogActions>
+        </Dialog>
       </div>
-        </DialogContent>
-        <DialogActions>
-          <div className="flex">
-            <button onClick={handleClose} className="flex items-center gap-2 rounded bg-primary px-4.5 py-2 font-medium text-white hover:bg-opacity-80">
-            Cancel
-            </button>
-          </div>
-          
-          <div className="flex">
-            <button onClick={handleClose}  className="flex items-center gap-2 rounded bg-primary px-4.5 py-2 font-medium text-white hover:bg-opacity-80">
-              Save
-            </button>
-          </div>
-        </DialogActions>
-      </Dialog>
-   </div>
     </DefaultLayout>
   );
 };
