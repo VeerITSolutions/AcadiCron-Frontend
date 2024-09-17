192df122b77f8189d580c83a0b5ce548bcc2dd99
pranay-lamse <pranay.l@veerit.com>
2024-09-17 18:34:59 +0530

    added

--- src/app/admin/staff/disablestafflist/page.tsx
+++ src/app/admin/staff/disablestafflist/page.tsx
@@ -1,16 +1,254 @@
-"use client"; // Add this at the top of the file
-import { useState } from "react";
-import { useRouter } from "next/navigation"; // This replaces `useRouter` from 'next/router' in the app directory
-import LogoutButton from "@/components/LogoutButton";
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import React from "react";
-
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import MUIDataTable from "mui-datatables";
+import { fetchStudentData } from "@/services/studentService";
+import styles from "./StudentDetails.module.css"; // Import CSS module
+import Loader from "@/components/common/Loader";
+import {
+  fetchsectionByClassData,
+  fetchsectionData,
+} from "@/services/sectionsService"; // Import your section API service
+import { getClasses } from "@/services/classesService"; // Import your classes API service
 
-const disablestafflist = () => {
-  const [error, setError] = useState<string | null>(null);
-  const router = useRouter();
+import {
+  Edit,
+  Delete,
+  Visibility,
+  TextFields,
+  AttachMoney,
+} from "@mui/icons-material";
+import IconButton from "@mui/material/IconButton";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+  TextField,
+} from "@mui/material";
+import { toast } from "react-toastify";
 
-  return <DefaultLayout>disablestafflist</DefaultLayout>;
+const columns = [
+  "Admission No",
+  "Student Name",
+  "Class",
+  "Category",
+  "Mobile Number",
+  "Action",
+];
+
+const options = {
+  filterType: "checkbox",
+  serverSide: true,
+  responsive: "standard",
+  filter: false, // Disable filter,
+  viewColumns: false, // Disable view columns button
 };
 
-export default disablestafflist;
+const StudentDetails = () => {
+  const [data, setData] = useState<Array<Array<string>>>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [totalCount, setTotalCount] = useState(0);
+  const [classes, setClassessData] = useState<Array<any>>([]);
+  const [section, setSections] = useState<Array<any>>([]);
+  const [selectedClass, setSelectedClass] = useState<string | undefined>(
+    undefined,
+  );
+  const [selectedSection, setSelectedSection] = useState<string | undefined>(
+    undefined,
+  );
+  const [keyword, setKeyword] = useState<string>("");
+  const router = useRouter();
+
+  const token = localStorage.getItem("authToken") || "";
+
+  const formatStudentData = (students: any[]) => {
+    return students.map((student: any) => [
+      student.admission_no,
+      `${student.firstname.trim()} ${student.lastname.trim()}`,
+      student.class_name || "N/A",
+      student.category_id,
+      student.mobileno,
+      <div key={student.id}>
+        <IconButton onClick={() => handleDelete(student.id)} aria-label="Show">
+          <Visibility />
+        </IconButton>
+        <IconButton onClick={() => handleEdit(student.id)} aria-label="Edit">
+          <Edit />
+        </IconButton>
+        <IconButton
+          onClick={() => handleAddFees(student.id)}
+          aria-label="Add Fee"
+        >
+          <AttachMoney />
+        </IconButton>
+      </div>,
+    ]);
+  };
+
+  const fetchData = async (
+    currentPage: number,
+    rowsPerPage: number,
+    selectedClass?: string,
+    selectedSection?: string,
+    keyword?: string,
+  ) => {
+    try {
+      // Pass selectedClass and selectedSection as parameters to filter data
+      const result = await fetchStudentData(
+        currentPage + 1,
+        rowsPerPage,
+        selectedClass,
+        selectedSection,
+        keyword,
+      );
+      setTotalCount(result.totalCount);
+      const formattedData = formatStudentData(result.data);
+      setData(formattedData);
+      setLoading(false);
+    } catch (error: any) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  const fetchClassesAndSections = async () => {
+    try {
+      const classesResult = await getClasses();
+      setClassessData(classesResult.data);
+
+      // Fetch sections if a class is selected
+      if (selectedClass) {
+        const sectionsResult = await fetchsectionByClassData(selectedClass);
+        setSections(sectionsResult.data);
+      } else {
+        setSections([]); // Clear sections if no class is selected
+      }
+    } catch (error: any) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  const handleDelete = async (id: number) => {
+    // Assuming id is the student_id
+    router.push(`/admin/student/${id}`);
+  };
+
+  const handleEdit = (id: number) => {
+    router.push(`/admin/student/edit/${id}`);
+  };
+
+  const handleAddFees = (id: number) => {
+    router.push(`/admin/student/fees/${id}`);
+  };
+
+  useEffect(() => {
+    fetchClassesAndSections(); // Fetch classes and sections on initial render
+  }, [selectedClass]);
+
+  useEffect(() => {
+    fetchData(page, rowsPerPage, selectedClass, selectedSection, keyword);
+  }, [page, rowsPerPage, selectedClass, selectedSection, keyword]);
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleRowsPerPageChange = (newRowsPerPage: number) => {
+    setRowsPerPage(newRowsPerPage);
+    setPage(0);
+  };
+
+  const handleClassChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedClass(event.target.value);
+    setPage(0);
+  };
+
+  const handleSectionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedSection(event.target.value);
+    setPage(0);
+  };
+
+  const handleKeywordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+
+  const handleSearch = () => {
+    setPage(0); // Reset to first page on search
+    fetchData(page, rowsPerPage, selectedClass, selectedSection, keyword);
+  };
+  const handleRefresh = () => {
+    setSelectedClass("");
+    setSelectedSection("");
+    setKeyword("");
+  };
+
+  if (loading) return <Loader />;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <DefaultLayout>
+      <div className={styles.filters}>
+        <div className={styles.filterGroup}>
+          <label className={styles.label}>
+            Role:
+            <select
+              value={selectedClass || ""}
+              onChange={handleClassChange}
+              className={styles.select}
+            >
+              <option value="">Select</option>
+              <option value="1">Admin</option>
+              <option value="2">Teacher</option>
+              <option value="3">Accountant</option>
+              <option value="4">Librarian</option>
+              <option value="6">Receptionist</option>
+              <option value="7">Super Admin</option>
+            </select>
+          </label>
+
+          <div className={styles.searchGroup}>
+            <label className={styles.label}>
+              Search By Keyword
+              <input
+                type="text"
+                placeholder="Search By Student Name, Roll Number, Enroll Number, National Id, Local Id Etc."
+                value={keyword}
+                onChange={handleKeywordChange}
+                className={styles.searchInput}
+              />
+            </label>
+            <button onClick={handleSearch} className={styles.searchButton}>
+              Search
+            </button>
+            <button onClick={handleRefresh} className={styles.searchButton}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+      <MUIDataTable
+        title={"Student Details"}
+        data={data}
+        columns={columns}
+        options={{
+          ...options,
+          count: totalCount,
+          page: page,
+          rowsPerPage: rowsPerPage,
+          onChangePage: handlePageChange,
+          onChangeRowsPerPage: handleRowsPerPageChange,
+        }}
+      />
+    </DefaultLayout>
+  );
+};
+
+export default StudentDetails;
