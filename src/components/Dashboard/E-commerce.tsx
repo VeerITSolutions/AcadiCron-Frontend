1dc4060e06cec9f716250aeb4085b6c735f57840
pranay-lamse <pranay.l@veerit.com>
2024-09-24 13:34:54 +0530

    added

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -31,7 +31,7 @@ const ECommerce: React.FC = () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Fees Awaiting Payment" total="$3.456K" rate="">
+        <CardDataStats title="Fees Awaiting Payment" total="₹3.456K" rate="">
           <svg
             className="fill-primary dark:fill-white"
             width="22"
