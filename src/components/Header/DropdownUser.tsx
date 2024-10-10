7bac3501d8127bedb6f83229f16211eb42ce36ca
pranay-lamse <pranay.l@veerit.com>
2024-10-10 13:09:22 +0530

    added

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -22,7 +22,7 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            {username || ""} {surname || ""}
+            {username ? username : ""} {surname ? surname : ""}
           </span>
           <span className="block text-xs">{roleName || ""}</span>
         </span>
