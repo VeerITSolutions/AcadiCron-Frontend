130a9c2238021410ccb242e7a50d181ec86d9613
pranay-lamse <pranay.l@veerit.com>
2024-10-28 16:05:48 +0530

    added

--- src/app/admin/student_admission/page.tsx
+++ src/app/admin/student_admission/page.tsx
@@ -1,11 +1,21 @@
 "use client"; // Add this at the top of the file
-import { useState, useContext } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation"; // This replaces `useRouter` from 'next/router' in the app directory
 import LogoutButton from "@/components/LogoutButton";
 import React from "react";
 
-const staffidcard = () => {
-  return <>Hello</>;
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import User from "@/components/User/User";
+
+const StudentAdmission = () => {
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  return (
+    <DefaultLayout>
+      <User />
+    </DefaultLayout>
+  );
 };
 
-export default staffidcard;
+export default StudentAdmission;
