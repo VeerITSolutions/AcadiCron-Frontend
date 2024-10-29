6acaefe8e7a21c47e73d7c7cd735a4a73a150a98
ManaliC0712 <manali.c@veerit.com>
2024-10-29 15:26:52 +0530

    added

--- src/app/admin/add_homework/page.tsx
+++ src/app/admin/add_homework/page.tsx
@@ -199,7 +199,7 @@ const StudentDetails = () => {
             <select
               value={selectedClass || ""}
               onChange={handleClassChange}
-              className={`${styles.select} dark:bg-boxdark dark:drop-shadow-none`}
+              className={`${styles.select} dark:bg-boxdark dark:drop-shadow-none dark:border-strokedark`}
             >
               <option value="">Select</option>
               {classes.map((cls) => (
@@ -214,7 +214,7 @@ const StudentDetails = () => {
             <select
               value={selectedSection || ""}
               onChange={handleSectionChange}
-              className={`${styles.select} dark:bg-boxdark dark:drop-shadow-none`}
+              className={`${styles.select} dark:bg-boxdark dark:drop-shadow-none dark:border-strokedark`}
               disabled={!selectedClass} // Disable section dropdown if no class is selected
             >
               <option value="">Select</option>
