44f631224ceeceb9bd740e9eba562fdb9c3b97a9
ManaliC0712 <manali.c@veerit.com>
2024-11-05 19:06:12 +0530

    Added

--- src/app/admin/leaverequest/leaverequest/page.tsx
+++ src/app/admin/leaverequest/leaverequest/page.tsx
@@ -458,19 +458,17 @@ const StudentDetails = () => {
                   />
                 </div>
 
-                {/* Attach Document */}
                 <div className="field">
-                  <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                    Attach Document
-                  </label>
-                  <input
-                    className="form-control mt-2 w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                    type="file"
-                    name="document_file"
-                    id="document_file"
-                    onChange={handleChange}
-                  />
-                </div>
+                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                Attach Document
+                </label>
+                <input
+                  type="file"
+                  className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-normal outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
+                  //onFileChange={handleFileChange}
+                />
+              </div>
+
 
                 {/* Send Message Button */}
                 <div className="col-span-full">
