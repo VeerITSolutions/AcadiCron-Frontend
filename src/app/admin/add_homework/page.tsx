4f707d80f5af4e7ec7dac5dfefbeb369905810bd
ManaliC0712 <manali.c@veerit.com>
2024-11-14 11:53:59 +0530

    Added homework

--- src/app/admin/add_homework/page.tsx
+++ src/app/admin/add_homework/page.tsx
@@ -14,11 +14,11 @@ import "flatpickr/dist/flatpickr.css"; // You can use other themes too
 import { getClasses } from "@/services/classesService";
 import { fetchsectionByClassData } from "@/services/sectionsService";
 import {
-  fetchLeaveData,
-  createLeave,
-  deleteLeaveData,
-  editLeaveData,
-} from "@/services/leaveService";
+  fetchHomeWorkData,
+  createHomeWork,
+  deleteHomeWorkData,
+  editHomeWorkData,
+} from "@/services/homeworkServices";
 
 import styles from "./StudentDetails.module.css"; // Import CSS module
 import Loader from "@/components/common/Loader";
@@ -93,7 +93,7 @@ const StudentDetails = () => {
 
   const handleDelete = async (id: number) => {
     try {
-      await deleteLeaveData(id);
+      await deleteHomeWorkData(id);
       toast.success("Delete successful");
       fetchData(page, rowsPerPage);
     } catch (error) {
@@ -144,7 +144,7 @@ const StudentDetails = () => {
     keyword?: string,
   ) => {
     try {
-      const result = await fetchLeaveData(
+      const result = await fetchHomeWorkData(
         currentPage + 1,
         rowsPerPage,
         selectedClass,
@@ -172,23 +172,17 @@ const StudentDetails = () => {
     try {
       let result;
       if (editing) {
-        result = await editLeaveData(
+        result = await editHomeWorkData(
           currentLeaveId!,
           formData.date,
           formData.leave_type_id,
-          formData.leave_from,
-          formData.leave_to,
-          formData.reason,
-          formData.document_file,
+         
         );
       } else {
-        result = await createLeave(
+        result = await createHomeWork(
           formData.date,
           formData.leave_type_id,
-          formData.leave_from,
-          formData.leave_to,
-          formData.reason,
-          formData.document_file,
+          
         );
       }
       if (result.success) {
@@ -420,31 +414,41 @@ const StudentDetails = () => {
             <div className="grid gap-5.5 p-6.5 sm:grid-cols-2">
       {/* Class */}
       <div className="field mb-3">
-        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-          Class <span className="required">*</span>
-        </label>
-        <select
-          name="modal_class_id"
-          className="form-control w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none"
-          
-        >
-          <option value="">Select</option>
-          <option value="1">Class 1</option>
-          <option value="2">Class 2</option>
-        </select>
+      <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                        Class:
+                      </label>
+                      <select
+                        value={selectedClass || ""}
+                        onChange={handleClassChange}
+                        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      >
+                        <option value="">Select</option>
+                        {classes.map((cls) => (
+                          <option key={cls.id} value={cls.id}>
+                            {cls.class}
+                          </option>
+                        ))}
+                      </select>
       </div>
 
       {/* Section */}
       <div className="field mb-3">
-        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-          Section <span className="required">*</span>
-        </label>
-        <select
-          name="modal_section_id"
-          className="form-control w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none">
-          <option value="">Select</option>
-          {/* You can dynamically populate this based on selected class */}
-        </select>
+      <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                        Section:
+                      </label>
+                      <select
+                        value={selectedSection || ""}
+                        onChange={handleSectionChange}
+                        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        disabled={!selectedClass} // Disable section dropdown if no class is selected
+                      >
+                        <option value="">Select</option>
+                        {section.map((sec) => (
+                          <option key={sec.section_id} value={sec.section_id}>
+                            {sec.section_name}
+                          </option>
+                        ))}
+                      </select>
       </div>
 
       {/* Subject Group */}
@@ -454,7 +458,7 @@ const StudentDetails = () => {
         </label>
         <select
           name="modal_subject_group_id"
-          className="form-control w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none">
+          className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary flatpickr-input">
           <option value="">Select</option>
           {/* Subject groups will be listed here */}
         </select>
@@ -467,7 +471,7 @@ const StudentDetails = () => {
         </label>
         <select
           name="modal_subject_id"
-          className="form-control w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none"
+          className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary flatpickr-input"
         
         >
           <option value="">Select</option>
@@ -477,43 +481,76 @@ const StudentDetails = () => {
 
       {/* Homework Date */}
       <div className="field mb-3">
-        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-          Homework Date <span className="required">*</span>
-        </label>
-        <div className="relative">
-          <Flatpickr
-            options={{
-              dateFormat: "m/d/Y",
-            }}
-            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none"
-          />
-        </div>
-      </div>
+                  <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                  Homework Date <span className="required">*</span>
+                  </label>
+                  <div className="relative">
+                    <Flatpickr
+                      /* value={formData.date} */
+                      onChange={handleDateChange}
+                      options={{
+                        dateFormat: "m/d/Y", // Customize date format if necessary
+                      }}
+                      className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      placeholder="mm/dd/yyyy"
+                    />
+                    <div className="pointer-events-none absolute inset-0 left-auto right-5 flex items-center">
+                      <svg
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M15.7504 2.9812H14.2879V2.36245C14.2879 2.02495 14.0066 1.71558 13.641 1.71558C13.2754 1.71558 12.9941 1.99683 12.9941 2.36245V2.9812H4.97852V2.36245C4.97852 2.02495 4.69727 1.71558 4.33164 1.71558C3.96602 1.71558 3.68477 1.99683 3.68477 2.36245V2.9812H2.25039C1.29414 2.9812 0.478516 3.7687 0.478516 4.75308V14.5406C0.478516 15.4968 1.26602 16.3125 2.25039 16.3125H15.7504C16.7066 16.3125 17.5223 15.525 17.5223 14.5406V4.72495C17.5223 3.7687 16.7066 2.9812 15.7504 2.9812ZM1.77227 8.21245H4.16289V10.9968H1.77227V8.21245ZM5.42852 8.21245H8.38164V10.9968H5.42852V8.21245ZM8.38164 12.2625V15.0187H5.42852V12.2625H8.38164V12.2625ZM9.64727 12.2625H12.6004V15.0187H9.64727V12.2625ZM9.64727 10.9968V8.21245H12.6004V10.9968H9.64727ZM13.8379 8.21245H16.2285V10.9968H13.8379V8.21245ZM2.25039 4.24683H3.71289V4.83745C3.71289 5.17495 3.99414 5.48433 4.35977 5.48433C4.72539 5.48433 5.00664 5.20308 5.00664 4.83745V4.24683H13.0504V4.83745C13.0504 5.17495 13.3316 5.48433 13.6973 5.48433C14.0629 5.48433 14.3441 5.20308 14.3441 4.83745V4.24683H15.7504C16.0316 4.24683 16.2566 4.47183 16.2566 4.75308V6.94683H1.77227V4.75308C1.77227 4.47183 1.96914 4.24683 2.25039 4.24683ZM1.77227 14.5125V12.2343H4.16289V14.9906H2.25039C1.96914 15.0187 1.77227 14.7937 1.77227 14.5125ZM15.7504 15.0187H13.8379V12.2625H16.2285V14.5406C16.2566 14.7937 16.0316 15.0187 15.7504 15.0187Z"
+                          fill="#64748B"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
 
       {/* Submit Date */}
-      <div className="field mb-3">
-        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-          Submission Date <span className="required">*</span>
-        </label>
-        <input
-          type="text"
-          name="submit_date"
-          className="form-control w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none"
-         
-        />
-      </div>
+      <div className="field mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                  Submission Date<span className="required">*</span>
+                  </label>
+                  <div className="relative">
+                    <Flatpickr
+                      /* value={formData.date} */
+                      onChange={handleDateChange}
+                      options={{
+                        dateFormat: "m/d/Y", // Customize date format if necessary
+                      }}
+                      className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      placeholder="mm/dd/yyyy"
+                    />
+                    <div className="pointer-events-none absolute inset-0 left-auto right-5 flex items-center">
+                      <svg
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M15.7504 2.9812H14.2879V2.36245C14.2879 2.02495 14.0066 1.71558 13.641 1.71558C13.2754 1.71558 12.9941 1.99683 12.9941 2.36245V2.9812H4.97852V2.36245C4.97852 2.02495 4.69727 1.71558 4.33164 1.71558C3.96602 1.71558 3.68477 1.99683 3.68477 2.36245V2.9812H2.25039C1.29414 2.9812 0.478516 3.7687 0.478516 4.75308V14.5406C0.478516 15.4968 1.26602 16.3125 2.25039 16.3125H15.7504C16.7066 16.3125 17.5223 15.525 17.5223 14.5406V4.72495C17.5223 3.7687 16.7066 2.9812 15.7504 2.9812ZM1.77227 8.21245H4.16289V10.9968H1.77227V8.21245ZM5.42852 8.21245H8.38164V10.9968H5.42852V8.21245ZM8.38164 12.2625V15.0187H5.42852V12.2625H8.38164V12.2625ZM9.64727 12.2625H12.6004V15.0187H9.64727V12.2625ZM9.64727 10.9968V8.21245H12.6004V10.9968H9.64727ZM13.8379 8.21245H16.2285V10.9968H13.8379V8.21245ZM2.25039 4.24683H3.71289V4.83745C3.71289 5.17495 3.99414 5.48433 4.35977 5.48433C4.72539 5.48433 5.00664 5.20308 5.00664 4.83745V4.24683H13.0504V4.83745C13.0504 5.17495 13.3316 5.48433 13.6973 5.48433C14.0629 5.48433 14.3441 5.20308 14.3441 4.83745V4.24683H15.7504C16.0316 4.24683 16.2566 4.47183 16.2566 4.75308V6.94683H1.77227V4.75308C1.77227 4.47183 1.96914 4.24683 2.25039 4.24683ZM1.77227 14.5125V12.2343H4.16289V14.9906H2.25039C1.96914 15.0187 1.77227 14.7937 1.77227 14.5125ZM15.7504 15.0187H13.8379V12.2625H16.2285V14.5406C16.2566 14.7937 16.0316 15.0187 15.7504 15.0187Z"
+                          fill="#64748B"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
 
       {/* Attach Document */}
       <div className="field mb-3">
-        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-          Attach Document
-        </label>
-        <input
-          type="file"
-          className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent"
-        
-        />
+     <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+      Attach Document
+      </label>
+      <input className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-normal outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary dark:text-white" type="file" />
       </div>
+     
 
       {/* Description */}
       <div className="field mb-3">
@@ -522,19 +559,19 @@ const StudentDetails = () => {
         </label>
         <textarea
           name="description"
-          className="form-control w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none"
+          className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
          
         />
       </div>
 
       {/* Submit Button */}
       <div className="col-span-full">
-        <button
-          type="submit"
-          className="w-full rounded-lg bg-primary py-3 px-5 text-white"
-        >
-          Submit
-        </button>
+      <button
+        type="submit"
+        className="rounded bg-[#1976D2] px-4 py-2 text-white hover:bg-[#155ba0] focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
+      >
+        Save
+      </button>
       </div>
     </div>
             </div>
