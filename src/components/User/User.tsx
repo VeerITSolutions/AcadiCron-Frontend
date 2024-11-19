18f132060a38ebd4bd1d7f811b65f21fd8282642
pranay-lamse <pranay.l@veerit.com>
2024-11-19 16:43:52 +0530

    added

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -226,7 +226,7 @@ const User = () => {
                 value={formData.section_id}
                 onChange={handleInputChange}
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                disabled={!selectedClass} // Disable section dropdown if no class is selected
+                /* disabled={!selectedClass} */ // Disable section dropdown if no class is selected
               >
                 <option value="">Select</option>
                 {section.map((sec) => (
