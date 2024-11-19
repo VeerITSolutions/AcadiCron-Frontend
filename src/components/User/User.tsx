bbef130970553265b7f8fe532885631b4a0b09cd
pranay-lamse <pranay.l@veerit.com>
2024-11-19 17:20:54 +0530

    added

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -394,7 +394,7 @@ const User = () => {
                 value={formData.admission_date}
                 onChange={handleInputChange}
                 placeholder=""
-                type="text"
+                type="date"
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
             </div>
