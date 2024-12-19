9600aebece106769e3149865d1a5326b19dff340
Nidhi Buradkar <nidhi.buradkar@gmail.com>
2024-12-19 13:24:21 +0530

    Your commit message

--- src/app/admin/income/page.tsx
+++ src/app/admin/income/page.tsx
@@ -287,9 +287,9 @@ search: false,
                  Date
                 </label>
                 <input
-                  name="Date"
-                  type="text"
-                  value={formData.code}
+                 id="date"
+                 name="date"
+                 type="date"
                   onChange={handleInputChange}
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 />
