d55199444ccd8d9e18ef599a2a953873f745c1a9
ManaliC0712 <manali.c@veerit.com>
2025-01-03 11:35:37 +0530

    Added

---
+++ src/services/incomeHeadService.ts
@@ -0,0 +1,37 @@
+import apiClient from "./apiClient";
+
+export const fetchIncomeHeadData = async (page?: number, perPage?: number, type?: any) => {
+  const response = await apiClient.get(`/income-head`, {
+    params: {
+      page,
+      perPage,
+      type
+    },
+  });
+  return response.data;
+};
+
+
+export const createIncomeHead = async (data: any): Promise<any> => {
+  try {
+    const response = await apiClient.post(`/income-head`, data);
+    return response.data;
+  } catch (error) {
+    console.error("An error occurred", error);
+    throw new Error("Failed to create student house");
+  }
+};
+
+
+
+// Delete a student category by ID
+export const deleteIncomeHead = async (id: number) => {
+  const response = await apiClient.delete(`/income-head/${id}`);
+  return response.data;
+};
+
+// Edit a student category by ID
+export const editIncomeHead = async (id: number, data: any) => {
+  const response = await apiClient.post(`/income-head/${id}`, data);
+  return response.data;
+};
