b23e7ca7ceff2031e52b7b16b7a13d292b1318de
ManaliC0712 <manali.c@veerit.com>
2025-01-03 17:22:30 +0530

    Added

---
+++ src/services/vehicleRouteService.ts
@@ -0,0 +1,37 @@
+import apiClient from "./apiClient";
+
+export const fetchVehicleRoutes = async (page?: number, perPage?: number) => {
+  const response = await apiClient.get(`/vehicle-routes`, {
+    params: {
+      page,
+      perPage,
+    },
+  });
+  return response.data;
+};
+
+
+export const createVehicleRoutes = async (data:any): Promise<any> => {
+  try {
+    const response = await apiClient.post("/vehicle-routes", data);
+    return response.data;
+  } catch (error) {
+    console.error("An error occurred", error);
+    throw new Error("Failed to create transport route");
+  }
+};
+
+
+
+// Delete a student category by ID
+export const deleteVehicleRoutes = async (id: number) => {
+  const response = await apiClient.delete(`/vehicle-routes/${id}`);
+  return response.data;
+};
+
+// Edit a student category by ID
+export const editVehicleRoutes = async (id: number, data: any) => {
+  // Create an object with the name field
+  const response = await apiClient.post(`/vehicle-routes/${id}`, data);
+  return response.data;
+};
