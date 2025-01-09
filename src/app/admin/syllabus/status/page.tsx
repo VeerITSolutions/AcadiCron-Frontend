8feff08d7e1806a8ae103e28fbd1f9af90c83d66
pranay-lamse <pranay.l@veerit.com>
2025-01-09 18:44:16 +0530

    added

--- src/app/admin/syllabus/status/page.tsx
+++ src/app/admin/syllabus/status/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
 import React from "react";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import MUIDataTable from "mui-datatables";
@@ -8,58 +7,20 @@ import { useGlobalState } from "@/context/GlobalContext";
 import { ThemeProvider } from "@mui/material/styles";
 import useColorMode from "@/hooks/useColorMode";
 import { darkTheme, lightTheme } from "@/components/theme/theme";
-import Close from "@mui/icons-material/Close"; // Import the Close icon
-import Flatpickr from "react-flatpickr";
 import "flatpickr/dist/themes/material_blue.css"; // Import the Flatpickr theme
 import "flatpickr/dist/flatpickr.css"; // You can use other themes too
 import { getClasses } from "@/services/classesService";
 import { fetchsectionByClassData } from "@/services/sectionsService";
-import {
-  fetchHomeWorkData,
-  createHomeWork,
-  deleteHomeWorkData,
-  editHomeWorkData,
-} from "@/services/homeworkServices";
+import { fetchHomeWorkData } from "@/services/homeworkServices";
 import { fetchSubjectGroupData } from "@/services/subjectGroupService";
 import { fetchSubjectData } from "@/services/subjectsService";
-
 import styles from "./StudentDetails.module.css"; // Import CSS module
 import Loader from "@/components/common/Loader";
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Button,
-  TextField,
-  IconButton,
-} from "@mui/material";
-import { toast } from "react-toastify";
-import { Delete, Edit, Visibility } from "@mui/icons-material";
-import { set } from "date-fns";
-
-const columns = [
-  "#",
-  "Lesson - Topic",
-  "Topic Completion Date",
-  "Status",
-  "Action",
-];
-
-const options = {
-  filterType: false,
-  serverSide: true,
-  responsive: "standard",
-  search: false,
-  selectableRows: "none",
-  filter: false,
-  viewColumns: false,
-};
 
 const StudentDetails = () => {
   const [data, setData] = useState<Array<Array<string>>>([]);
   const { themType, setThemType } = useGlobalState(); //
-  const [dataleavetype, setLeaveTypeData] = useState<Array<any>>([]);
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [page, setPage] = useState(0);
@@ -70,11 +31,6 @@ const StudentDetails = () => {
   const [section, setSections] = useState<Array<any>>([]);
   const [subjectGroup, setSubjectGroup] = useState<Array<any>>([]);
   const [subject, setSubject] = useState<Array<any>>([]);
-
-  const [classes2, setClassessData2] = useState<Array<any>>([]);
-  const [section2, setSections2] = useState<Array<any>>([]);
-  const [subjectGroup2, setSubjectGroup2] = useState<Array<any>>([]);
-  const [subject2, setSubject2] = useState<Array<any>>([]);
 
   const [selectedClass, setSelectedClass] = useState<string | undefined>(
     undefined,
@@ -89,31 +45,25 @@ const StudentDetails = () => {
     undefined,
   );
 
-  const [selectedClass2, setSelectedClass2] = useState<string | undefined>(
-    undefined,
-  );
-  const [selectedSection2, setSelectedSection2] = useState<string | undefined>(
-    undefined,
-  );
-  const [selectedSubjectGroup2, setSelectedSubjectGroup2] = useState<
-    string | undefined
-  >(undefined);
-  const [selectedSubject2, setSelectedSubject2] = useState<string | undefined>(
-    undefined,
-  );
-
   const [keyword, setKeyword] = useState<string>("");
-  const [colorMode, setColorMode] = useColorMode();
-  const [formData, setFormData] = useState({
-    homework_date: null as Date | null,
-    submit_date: null as Date | null,
-    description: "",
-    document: null,
-  });
-  const [editing, setEditing] = useState(false); // Add state for editing
-  const [currentLeaveId, setCurrentLeaveId] = useState<number | null>(null); // ID of the leave being
 
-  const [open, setOpen] = useState(false);
+  const columns = [
+    "#",
+    "Lesson - Topic",
+    "Topic Completion Date",
+    "Status",
+    "Action",
+  ];
+
+  const options = {
+    filterType: false,
+    serverSide: true,
+    responsive: "standard",
+    search: false,
+    selectableRows: "none",
+    filter: false,
+    viewColumns: false,
+  };
 
   /* use Effect  */
 
@@ -132,9 +82,7 @@ const StudentDetails = () => {
     rowsPerPage,
     selectedClass,
     selectedSection,
-    selectedSection2,
-    selectedSubjectGroup,
-    selectedSubjectGroup2,
+
     selectedSubject,
     keyword,
   ]);
@@ -143,54 +91,7 @@ const StudentDetails = () => {
     fetchClassesAndSections(); // Fetch classes and sections on initial render
   }, [selectedClass]);
 
-  useEffect(() => {
-    fetchClassesAndSections2(); // Fetch classes and sections on initial render
-  }, [selectedClass2, selectedSection2]);
-
   /* use effect End  */
-
-  const handleDelete = async (id: number) => {
-    try {
-      await deleteHomeWorkData(id);
-      toast.success("Delete successful");
-      fetchData(page, rowsPerPage);
-    } catch (error) {
-      console.error("Delete failed", error);
-      toast.error("Delete failed");
-    }
-  };
-
-  const handleDateChange = (selectedDates: Date[], name: string) => {
-    if (selectedDates.length > 0) {
-      const formattedDate = selectedDates[0].toLocaleDateString("en-CA"); // Format to YYYY-MM-DD
-
-      setFormData((prevState) => ({
-        ...prevState,
-        [name]: formattedDate, // Update the specific field dynamically
-      }));
-    }
-  };
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, files } = e.target;
-    const file = files ? files[0] : null;
-
-    if (file && name) {
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: file, // Dynamically set the file in formData using the input's name attribute
-      }));
-    }
-  };
-
-  const formatDate = (dateString: any) => {
-    if (!dateString) return "N/A"; // Handle null/undefined dates
-    const date = new Date(dateString);
-    if (isNaN(date.getTime())) return "N/A"; // Handle invalid dates
-    return new Intl.DateTimeFormat("en-US", { dateStyle: "medium" }).format(
-      date,
-    );
-  };
 
   const formatStudentData = (students: any[]) => {
     return students.map((student: any) => [
@@ -228,8 +129,6 @@ const StudentDetails = () => {
       const classesResult = await getClasses();
       setClassessData(classesResult.data);
 
-      setClassessData2(classesResult.data);
-
       /* call condtion wise  */
       if (selectedClass && selectedSection) {
         const subjectgroupresult = await fetchSubjectGroupData(
@@ -250,15 +149,6 @@ const StudentDetails = () => {
         setSubject(subjectresult.data);
       }
 
-      if (selectedSubjectGroup2) {
-        const subjectresult2 = await fetchSubjectData(
-          "",
-          "",
-          selectedSubjectGroup2,
-        );
-        setSubject2(subjectresult2.data);
-      }
-
       /* call condtin wise end  */
 
       setLoading(false);
@@ -266,97 +156,6 @@ const StudentDetails = () => {
       setError(error.message);
       setLoading(false);
     }
-  };
-
-  const handleSave = async () => {
-    try {
-      let result;
-      if (editing) {
-        result = await editHomeWorkData(
-          currentLeaveId,
-          selectedClass2,
-          selectedSection2,
-          selectedSubjectGroup2,
-          selectedSubject2,
-          formData.homework_date,
-          formData.submit_date,
-          formData.document,
-          formData.description,
-        );
-
-        fetchData(page, rowsPerPage);
-      } else {
-        result = await createHomeWork(
-          selectedClass2,
-          selectedSection2,
-          selectedSubjectGroup2,
-          selectedSubject2,
-          formData.homework_date,
-          formData.submit_date,
-          formData.document,
-          formData.description,
-        );
-        fetchData(page, rowsPerPage); // Refresh data after submit
-      }
-      if (result.success) {
-        toast.success(
-          editing
-            ? "Homework updated successfully"
-            : "Homework applied successfully",
-        );
-        setFormData({
-          homework_date: null as Date | null,
-          submit_date: null as Date | null,
-          description: "",
-          document: null,
-        });
-        setSelectedClass2("");
-        setSelectedSection2("");
-        setSelectedSubjectGroup2("");
-        setSelectedSubject2("");
-        setOpen(false); // Close the modal
-        setEditing(false); // Reset editing state
-        fetchData(page, rowsPerPage); // Refresh data after submit
-      } else {
-        toast.error("Failed to save leave");
-      }
-    } catch (error) {
-      console.error("An error occurred", error);
-      toast.error("An error occurred while saving leave");
-    }
-  };
-
-  const handleEdit = async (id: number, data: any) => {
-    setEditing(true);
-    setCurrentLeaveId(id);
-
-    try {
-      console.log(data);
-      setFormData(data);
-      setSelectedClass2(data.class_id);
-      setSelectedSection2(data.section_id);
-      setSelectedSubjectGroup2(data.subject_groups_id);
-      setSelectedSubject2(data.subject_id);
-
-      setLoading(false);
-    } catch (error: any) {
-      setError(error.message);
-      setLoading(false);
-    }
-
-    setOpen(true); // Open the modal
-  };
-
-  const handleInputChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-    >,
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value, // For regular inputs like text or selects
-    }));
   };
 
   const handlePageChange = (newPage: number) => {
@@ -383,28 +182,6 @@ const StudentDetails = () => {
     setSelectedSubject(event.target.value);
   };
 
-  const handleSectionChange2 = (
-    event: React.ChangeEvent<HTMLSelectElement>,
-  ) => {
-    setSelectedSection2(event.target.value);
-  };
-
-  const handleSubjectGroupChange2 = (
-    event: React.ChangeEvent<HTMLSelectElement>,
-  ) => {
-    setSelectedSubjectGroup2(event.target.value);
-  };
-
-  const handleSubjectChange2 = (
-    event: React.ChangeEvent<HTMLSelectElement>,
-  ) => {
-    setSelectedSubject2(event.target.value);
-  };
-
-  const handleKeywordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setKeyword(event.target.value);
-  };
-
   const handleSearch = () => {
     setPage(0); // Reset to first page on search
     fetchData(
@@ -418,33 +195,9 @@ const StudentDetails = () => {
     );
   };
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setFormData({
-      homework_date: null as Date | null,
-      submit_date: null as Date | null,
-      description: "",
-      document: null,
-    });
-
-    setSelectedClass2("");
-    setSelectedSection2("");
-    setSelectedSubjectGroup2("");
-    setSelectedSubject2("");
-    setOpen(false);
-    setEditing(false); // Reset editing state
-  };
-
   const handleClassChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedClass(event.target.value);
     setPage(0);
-  };
-
-  const handleClassChange2 = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedClass2(event.target.value);
   };
 
   const handleRefresh = () => {
@@ -463,31 +216,6 @@ const StudentDetails = () => {
         setSections(sectionsResult.data);
       } else {
         setSections([]); // Clear sections if no class is selected
-      }
-    } catch (error: any) {
-      setError(error.message);
-      setLoading(false);
-    }
-  };
-
-  const fetchClassesAndSections2 = async () => {
-    try {
-      // Fetch sections if a class is selected
-      if (selectedClass2) {
-        const sectionsResult = await fetchsectionByClassData(selectedClass2);
-        setSections2(sectionsResult.data);
-      } else {
-        setSections2([]); // Clear sections if no class is selected
-      }
-      if (selectedClass2 && selectedSection2) {
-        const subjectgroupresult = await fetchSubjectGroupData(
-          "",
-          "",
-          selectedClass2,
-          selectedSection2,
-        );
-
-        setSubjectGroup2(subjectgroupresult.data);
       }
     } catch (error: any) {
       setError(error.message);
