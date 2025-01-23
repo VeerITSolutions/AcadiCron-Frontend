b9fa4fb338adb3308cb9fa603a022660b4899295
pranay-lamse <pranay.l@veerit.com>
2025-01-23 17:17:03 +0530

    added

--- src/app/admin/stdtransfer/page.tsx
+++ src/app/admin/stdtransfer/page.tsx
@@ -31,6 +31,7 @@ import {
 } from "@mui/material";
 import { toast } from "react-toastify";
 import { useLoginDetails } from "@/store/logoStore";
+import { fetchSession } from "@/services/session";
 const columns = [
   "Admission No",
   "Student Name",
@@ -58,14 +59,28 @@ const StudentDetails = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [totalCount, setTotalCount] = useState(0);
+  const [allSession, setAllSession] = useState<Array<any>>([]);
+
+  const [promotedsession, setPromotedSession] = useState<string>("");
   const [selectedClass, setSelectedClass] = useState<string | undefined>(
     undefined,
   );
+
   const [selectedSection, setSelectedSection] = useState<string | undefined>(
     undefined,
   );
   const [classes, setClassessData] = useState<Array<any>>([]);
   const [section, setSections] = useState<Array<any>>([]);
+
+  const [selectedClass2, setSelectedClass2] = useState<string | undefined>(
+    undefined,
+  );
+  const [selectedSection2, setSelectedSection2] = useState<string | undefined>(
+    undefined,
+  );
+  const [classes2, setClassessData2] = useState<Array<any>>([]);
+  const [section2, setSections2] = useState<Array<any>>([]);
+
   const [keyword, setKeyword] = useState<string>("");
   const router = useRouter();
   const [colorMode, setColorMode] = useColorMode();
@@ -96,22 +111,32 @@ const StudentDetails = () => {
     selectedSection?: string,
     keyword?: string,
   ) => {
-    try {
-      const result = await fetchStudentData(
-        currentPage + 1,
-        rowsPerPage,
-        selectedClass,
-        selectedSection,
-        keyword,
-        selectedSessionId,
-      );
-      setTotalCount(result.totalCount);
-      const formattedData = formatStudentData(result.data);
-      setData(formattedData);
-      setLoading(false);
-    } catch (error: any) {
-      setError(error.message);
-      setLoading(false);
+    if (allSession.length == 0) {
+      try {
+        const classesResult = await fetchSession();
+        setAllSession(classesResult.data);
+      } catch (error: any) {
+        console.error("Error fetching sessions:", error);
+      }
+    }
+    if (selectedClass) {
+      try {
+        const result = await fetchStudentData(
+          currentPage + 1,
+          rowsPerPage,
+          selectedClass,
+          selectedSection,
+          keyword,
+          selectedSessionId,
+        );
+        setTotalCount(result.totalCount);
+        const formattedData = formatStudentData(result.data);
+        setData(formattedData);
+        setLoading(false);
+      } catch (error: any) {
+        setError(error.message);
+        setLoading(false);
+      }
     }
   };
   const handleDelete = async (id: number) => {
@@ -149,6 +174,18 @@ const StudentDetails = () => {
     setPage(0);
   };
 
+  const handleClassChange2 = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedClass2(event.target.value);
+    setPage(0);
+  };
+
+  const handleSectionChange2 = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setSelectedSection2(event.target.value);
+    setPage(0);
+  };
+
   const handleKeywordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(event.target.value);
   };
@@ -161,16 +198,22 @@ const StudentDetails = () => {
     setSelectedClass("");
     setSelectedSection("");
     setKeyword("");
+    setPromotedSession("");
+    setData([]);
   };
 
   useEffect(() => {
     fetchClassesAndSections(); // Fetch classes and sections on initial render
-  }, [selectedClass]);
-
+  }, [selectedClass, selectedClass2]);
+  const handleSessionChange = (value: string) => {
+    setPromotedSession(value);
+  };
   const fetchClassesAndSections = async () => {
     try {
       const classesResult = await getClasses();
       setClassessData(classesResult.data);
+
+      setClassessData2(classesResult.data);
 
       // Fetch sections if a class is selected
       if (selectedClass) {
@@ -178,6 +221,13 @@ const StudentDetails = () => {
         setSections(sectionsResult.data);
       } else {
         setSections([]); // Clear sections if no class is selected
+      }
+
+      if (selectedClass2) {
+        const sectionsResult = await fetchsectionByClassData(selectedClass2);
+        setSections2(sectionsResult.data);
+      } else {
+        setSections2([]); // Clear sections if no class is selected
       }
     } catch (error: any) {
       setError(error.message);
@@ -189,146 +239,152 @@ const StudentDetails = () => {
 
   return (
     <DefaultLayout>
-      <div className="bg-white dark:bg-form-dark dark:text-white p-4 mb-5 dark:bg-boxdark dark:drop-shadow-none dark:border-strokedark">
-      <div className={styles.filters}>
-        <div>
-          <h6 className="font-satoshi text-[1.25rem] font-semibold leading-[1.75rem] dark:text-white">
-            {" "}
-            Select Criteria{" "}
-          </h6>
-        </div>
-        <div className="flex w-full gap-4">
-        <label className={`${styles.label} w-1/2`}>
-    Class:
-    <select
-      value={selectedClass || ""}
-      onChange={handleClassChange}
-      className={`${styles.select} w-full rounded-lg border-stroke outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary`}
-    >
-      <option value="">Select</option>
-      {classes.map((cls) => (
-        <option key={cls.id} value={cls.id}>
-          {cls.class}
-        </option>
-      ))}
-    </select>
-        </label>
-        <label className={`${styles.label} w-1/2`}>
-          Section:
-          <select
-            value={selectedSection || ""}
-            onChange={handleSectionChange}
-            className={`${styles.select} w-full rounded-lg border-stroke outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary`}
-            disabled={!selectedClass} // Disable section dropdown if no class is selected
-          >
-            <option value="">Select</option>
-            {section.map((sec) => (
-              <option key={sec.section_id} value={sec.section_id}>
-                {sec.section_name}
-              </option>
-            ))}
-          </select>
-        </label>
-     </div>
+      <div className="dark:bg-form-dark mb-5 bg-white p-4 dark:border-strokedark dark:bg-boxdark dark:text-white dark:drop-shadow-none">
+        <div className={styles.filters}>
+          <div>
+            <h6 className="font-satoshi text-[1.25rem] font-semibold leading-[1.75rem] dark:text-white">
+              {" "}
+              Select Criteria{" "}
+            </h6>
+          </div>
+          <div className="flex w-full gap-4">
+            <label className={`${styles.label} w-1/2`}>
+              Class:
+              <select
+                value={selectedClass || ""}
+                onChange={handleClassChange}
+                className={`border-gray-300 w-full rounded-lg border p-3 focus:border-blue-500 focus:outline-none dark:border-strokedark dark:bg-boxdark`}
+              >
+                <option value="">Select</option>
+                {classes.map((cls) => (
+                  <option key={cls.id} value={cls.id}>
+                    {cls.class}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <label className={`${styles.label} w-1/2`}>
+              Section:
+              <select
+                value={selectedSection || ""}
+                onChange={handleSectionChange}
+                className={`border-gray-300 w-full rounded-lg border p-3 focus:border-blue-500 focus:outline-none dark:border-strokedark dark:bg-boxdark`}
+                disabled={!selectedClass} // Disable section dropdown if no class is selected
+              >
+                <option value="">Select</option>
+                {section.map((sec) => (
+                  <option key={sec.section_id} value={sec.section_id}>
+                    {sec.section_name}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
 
-        {/*  <div className={styles.searchGroup}>
+          {/*  <div className={styles.searchGroup}>
 
         </div> */}
-      </div>
-
-      <div className={`${styles.filters} mt-6`}>
-      <div>
-        <h6 className="font-satoshi text-[1.25rem] font-semibold leading-[1.75rem] dark:text-white">
-          Promote Students In Next Session
-        </h6>
-      </div>
-      <div className="flex gap-4 items-center flex-wrap lg:flex-nowrap">
-        <label className={`${styles.label} flex flex-col lg:w-1/5`}>
-          Promote In Session:
-          <select
-            value={selectedClass || ""}
-            onChange={handleClassChange}
-            className={`${styles.select} rounded-lg border-stroke outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary`}
-          >
-            <option value="">Select</option>
-            <option value="Class1">2017-18</option>
-            <option value="Class2">2018-19</option>
-            {/* Add more class options here */}
-          </select>
-        </label>
-
-        <label className={`${styles.label} flex flex-col lg:w-1/5`}>
-          Class:
-          <select
-            value={selectedClass || ""}
-            onChange={handleClassChange}
-            className={`${styles.select} rounded-lg border-stroke outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary`}
-          >
-            <option value="">Select</option>
-            {classes.map((cls) => (
-              <option key={cls.id} value={cls.id}>
-                {cls.class}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <label className={`${styles.label} flex flex-col lg:w-1/5`}>
-          Section:
-          <select
-            value={selectedSection || ""}
-            onChange={handleSectionChange}
-            className={`${styles.select} rounded-lg border-stroke outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary`}
-            disabled={!selectedClass} // Disable section dropdown if no class is selected
-          >
-            <option value="">Select</option>
-            {section.map((sec) => (
-              <option key={sec.section_id} value={sec.section_id}>
-                {sec.section_name}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        {/* Adjusting the Search and Reset buttons */}
-        <div className={`${styles.searchGroup} flex gap-2 pt-0 lg:pt-7`}>
-          <button
-            onClick={handleSearch}
-            className={`${styles.searchButton} px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary-dark transition`}
-          >
-            Search
-          </button>
-          <button
-            onClick={handleRefresh}
-            className={`${styles.searchButton} px-4 py-2 bg-secondary text-white rounded-lg hover:bg-secondary-dark transition`}
-          >
-            Reset
-          </button>
         </div>
-  </div>
-</div>
 
+        <div className={`${styles.filters} mt-6`}>
+          <div>
+            <h6 className="font-satoshi text-[1.25rem] font-semibold leading-[1.75rem] dark:text-white">
+              Promote Students In Next Session
+            </h6>
+          </div>
+          <div className="flex flex-wrap items-center gap-4 lg:flex-nowrap">
+            <label className={`${styles.label} flex flex-col lg:w-1/5`}>
+              Promote In Session:
+              <select
+                id="session"
+                className="border-gray-300 w-full rounded-lg border p-3 focus:border-blue-500 focus:outline-none dark:border-strokedark dark:bg-boxdark"
+                value={promotedsession}
+                onChange={(e) => handleSessionChange(e.target.value)} // Call function when session changes
+              >
+                <option value="">Select</option>
+                {allSession?.map((cls: any) => (
+                  <option key={cls.id} value={cls.id}>
+                    {cls.session}
+                  </option>
+                ))}
+              </select>
+            </label>
 
+            <label className={`${styles.label} flex flex-col lg:w-1/5`}>
+              Class:
+              <select
+                value={selectedClass2 || ""}
+                onChange={handleClassChange2}
+                className={`border-gray-300 w-full rounded-lg border p-3 focus:border-blue-500 focus:outline-none dark:border-strokedark dark:bg-boxdark`}
+              >
+                <option value="">Select</option>
+                {classes2.map((cls) => (
+                  <option key={cls.id} value={cls.id}>
+                    {cls.class}
+                  </option>
+                ))}
+              </select>
+            </label>
+
+            <label className={`${styles.label} flex flex-col lg:w-1/5`}>
+              Section:
+              <select
+                value={selectedSection2 || ""}
+                onChange={handleSectionChange2}
+                className={`border-gray-300 w-full rounded-lg border p-3 focus:border-blue-500 focus:outline-none dark:border-strokedark dark:bg-boxdark`}
+                disabled={!selectedClass2} // Disable section dropdown if no class is selected
+              >
+                <option value="">Select</option>
+                {section2.map((sec) => (
+                  <option key={sec.section_id} value={sec.section_id}>
+                    {sec.section_name}
+                  </option>
+                ))}
+              </select>
+            </label>
+
+            {/* Adjusting the Search and Reset buttons */}
+            <div className={`${styles.searchGroup} flex gap-2 pt-0 lg:pt-7`}>
+              <button
+                onClick={handleSearch}
+                className={`${styles.searchButton} hover:bg-primary-dark rounded-lg bg-primary px-4 py-2 text-white transition`}
+              >
+                Search
+              </button>
+              <button
+                onClick={handleRefresh}
+                className={`${styles.searchButton} hover:bg-secondary-dark rounded-lg bg-secondary px-4 py-2 text-white transition`}
+              >
+                Reset
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-
-      {loading ? (
-        <Loader />
+      {data.length > 0 ? (
+        <>
+          {loading ? (
+            <Loader />
+          ) : (
+            <ThemeProvider theme={themType === "dark" ? darkTheme : lightTheme}>
+              <MUIDataTable
+                title={"Student List"}
+                data={data}
+                columns={columns}
+                options={{
+                  ...options,
+                  count: totalCount,
+                  page: page,
+                  rowsPerPage: rowsPerPage,
+                  onChangePage: handlePageChange,
+                  onChangeRowsPerPage: handleRowsPerPageChange,
+                }}
+              />
+            </ThemeProvider>
+          )}
+        </>
       ) : (
-        <ThemeProvider theme={themType === "dark" ? darkTheme : lightTheme}>
-          <MUIDataTable
-            title={" Student List "}
-            data={data}
-            columns={columns}
-            options={{
-              ...options,
-              count: totalCount,
-              page: page,
-              rowsPerPage: rowsPerPage,
-              onChangePage: handlePageChange,
-              onChangeRowsPerPage: handleRowsPerPageChange,
-            }}
-          />
-        </ThemeProvider>
+        ""
       )}
     </DefaultLayout>
   );
