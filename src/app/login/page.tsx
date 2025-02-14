f21ef21a8aa5d9e117dd7b2bbb59adc3d20e59aa
pranay-lamse <pranay.l@veerit.com>
2025-02-14 13:05:01 +0530

    added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -328,10 +328,10 @@ const LoginPage = () => {
                   </label>
                   <div className="relative">
                     <input
+                      autoComplete="off"
                       type="text"
                       placeholder="Enter your email Or Username"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                      /* value="pranay.l@veerit.com" */
                       onChange={(e) => setEmail(e.target.value)}
                     />
 
@@ -361,6 +361,7 @@ const LoginPage = () => {
                   </label>
                   <div className="relative">
                     <input
+                      autoComplete="off"
                       type="password"
                       placeholder=""
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
