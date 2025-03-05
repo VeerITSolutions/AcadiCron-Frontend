d363245508b90a50a51a122f6bb9994c61423dd8
pranay-lamse <pranay.l@veerit.com>
2025-03-05 14:48:54 +0530

    addd

---
+++ src/app/admin/notic_ecampus_message_board/add/page.tsx
@@ -0,0 +1,400 @@
+"use client";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import styles from "./User.module.css"; // Assuming this has your styles
+import {
+  createNotification,
+  editNotificationData,
+  fetchNotificationData,
+  deleteNotificationData,
+} from "@/services/notificationService";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import { IconButton } from "@mui/material";
+import { Delete, Edit } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
+import { useLoginDetails } from "@/store/logoStore";
+import { useGlobalState } from "@/context/GlobalContext";
+import { Editor } from "@tinymce/tinymce-react";
+// Dynamic import for ReactQuill
+
+const NoticeForm = () => {
+  // Quill editor modules
+
+  const handleInputChange = (
+    e:
+      | React.ChangeEvent<
+          HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+        >
+      | { target: { name: string; value: string } }, // Extend type to include Quill's custom events
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  // Function to handle file input changes
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, files } = e.target;
+    const file = files ? files[0] : null;
+
+    if (file && name) {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: file, // Dynamically set the file in formData using the input's name attribute
+      }));
+    }
+  };
+
+  const [content, setContent] = useState("");
+
+  const handleEditorChange = (newContent: any) => {
+    setContent(newContent);
+    console.log("Content was updated:", newContent);
+  };
+
+  const [error, setError] = useState<string | null>(null);
+
+  const [roleId, setRoleId] = useState("");
+  const [data, setData] = useState<Array<Array<any>>>([]);
+  const { themType, setThemType } = useGlobalState();
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [roleName, setRoleName] = useState("");
+
+  const [totalCount, setTotalCount] = useState(0);
+  const [value, setValue] = useState<string>(""); // State for message content
+  const [formData, setFormData] = useState({
+    title: "",
+    publish_date: "",
+    date: "",
+    message: "",
+    visible_student: "",
+    visible_staff: "",
+    visible_parent: "",
+    created_by: "",
+    created_id: "",
+    is_active: "",
+    created_at: "",
+    updated_at: "",
+    path: "",
+    class_id: "",
+    secid: "",
+  });
+  const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
+  const [editCategoryId, setEditCategoryId] = useState<number | null>(null);
+
+  const fetchData = async (currentPage: number, rowsPerPage: number) => {
+    try {
+      const result = await fetchNotificationData(currentPage + 1, rowsPerPage);
+      setTotalCount(result.totalCount);
+      setLoading(false);
+    } catch (error: any) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+  let roleIdget = useLoginDetails((state) => state.roleId);
+  let username = useLoginDetails((state) => state.username);
+  let surname = useLoginDetails((state) => state.surname);
+  let roleNameget = useLoginDetails((state) => state.roleName);
+  let isSuperAdmin = useLoginDetails((state) => state.isSuperAdmin);
+  let selectedSessionId = useLoginDetails((state) => state.selectedSessionId);
+  useEffect(() => {
+    if (roleNameget) {
+      setRoleName(roleNameget);
+    }
+    if (roleIdget) {
+      setRoleId(roleIdget);
+    }
+
+    fetchData(page, rowsPerPage);
+  }, [page, rowsPerPage]);
+
+  const handleSave = async () => {
+    try {
+      setLoading(true);
+      const data = {
+        ...formData,
+        created_by: roleName,
+        created_id: roleId,
+      };
+
+      const response = await createNotification(data);
+
+      if (response.status == 200) {
+        toast.success("Added successful");
+        router.push(`/admin/notic_board`);
+      } else {
+        toast.error("Error Edit data");
+      }
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <DefaultLayout>
+      <div className="student_admission_form">
+        <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between"></div>
+        <div className="flex flex-col gap-9">
+          <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+            <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
+              <h3 className="font-medium text-black dark:text-white">
+                Compose New Message
+              </h3>
+            </div>
+            <div className="grid grid-cols-3 gap-6 pl-6 pr-6 pt-6">
+              {/* First Column */}
+              <div className="col-span-2">
+                <div className="field mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                    Title <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleInputChange}
+                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                  />
+                </div>
+
+                <div className="field mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                    Message
+                  </label>
+                  <Editor
+                    key={themType}
+                    apiKey="3gpvx3o0o5bmecnx6svljl3kl3kgvnz24v0jt4x1k242uey4" // Replace with your TinyMCE API key
+                    initialValue="<p>Start typing...</p>"
+                    value={content}
+                    onEditorChange={handleEditorChange}
+                    init={{
+                      height: 300,
+                      menubar: false,
+                      plugins: [
+                        "advlist autolink lists link image charmap print preview anchor",
+                        "searchreplace visualblocks code fullscreen",
+                        "insertdatetime media table paste code help wordcount",
+                      ],
+                      toolbar:
+                        "undo redo | formatselect | bold italic backcolor | \
+          alignleft aligncenter alignright alignjustify | \
+          bullist numlist outdent indent | removeformat | help",
+                      content_style:
+                        themType === "dark"
+                          ? "body { background-color: #24303F; color: #fff; font-family: Arial, sans-serif; }"
+                          : "body { background-color: #fff; color: #000; font-family: Arial, sans-serif; }",
+                      skin: themType === "dark" ? "oxide-dark" : "oxide", // Apply dark/light skin
+                      content_css: themType === "dark" ? "oxide-dark" : "oxide", // Apply dark/light content styling
+                    }}
+                  />
+                </div>
+
+                <div className="field mb-6 pt-9">
+                  <label className="block text-sm font-medium text-black dark:text-white">
+                    Upload File
+                  </label>
+                  <input
+                    className="form-control mt-2 w-full"
+                    type="file"
+                    accept="image/*"
+                    name="path"
+                    onChange={handleFileChange}
+                  />
+                </div>
+              </div>
+
+              {/* Second Column */}
+              <div className="col-span-1">
+                <div className="field mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                    Notice Date <span className="required">*</span>
+                  </label>
+                  <input
+                    id="date"
+                    name="date"
+                    value={
+                      formData.date || new Date().toISOString().split("T")[0]
+                    }
+                    onChange={handleInputChange}
+                    type="date"
+                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                  />
+                </div>
+
+                <div className="field mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                    Publish On
+                  </label>
+                  <input
+                    id="publish_date"
+                    name="publish_date"
+                    value={
+                      formData.publish_date ||
+                      new Date().toISOString().split("T")[0]
+                    }
+                    onChange={handleInputChange}
+                    type="date"
+                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                  />
+                </div>
+
+                <div className="field mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                    Message To
+                  </label>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="student"
+                        name="message_to"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Student
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="parent"
+                        name="message_to"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Parent
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="admin"
+                        name="message_to"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Admin
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="teacher"
+                        name="message_to"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Teacher
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="accountant"
+                        name="message_to"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Accountant
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="librarian"
+                        name="message_to"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Librarian
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="receptionist"
+                        name="message_to"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Receptionist
+                    </label>
+                  </div>
+                </div>
+
+                <div className="field mb-6">
+                  <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                    Multiple Select
+                  </label>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="class1"
+                        name="classes"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Class 1 to 2
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="class2"
+                        name="classes"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Class 3 to 4
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="class3"
+                        name="classes"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Class 5 to 7
+                    </label>
+                  </div>
+                  <div>
+                    <label className="radio-inline">
+                      <input
+                        type="checkbox"
+                        value="class4"
+                        name="classes"
+                        onChange={handleInputChange}
+                      />{" "}
+                      Class 8 to 9
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="px-6.5 py-4">
+              <button
+                onClick={handleSave}
+                className="flex items-center gap-2 rounded bg-primary px-4.5 py-2 font-medium text-white hover:bg-opacity-80"
+              >
+                Send
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default NoticeForm;
---
+++ src/app/admin/notic_ecampus_message_board/page.tsx
@@ -0,0 +1,210 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import LogoutButton from "@/components/LogoutButton";
+import {
+  fetchNotificationData,
+  deleteNotificationData,
+} from "@/services/notificationService";
+import React from "react";
+import toast from "react-hot-toast";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import Link from "next/link";
+import {
+  Edit,
+  Delete,
+  Visibility,
+  TextFields,
+  AttachMoney,
+} from "@mui/icons-material";
+import IconButton from "@mui/material/IconButton";
+import { useGlobalState } from "@/context/GlobalContext";
+import Image from "next/image";
+const StudentDetails = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [data, setData] = useState<Array<Array<string>>>([]);
+  const { themType, setThemType } = useGlobalState(); //
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [totalCount, setTotalCount] = useState(0);
+  const [selectedClass, setSelectedClass] = useState<string | undefined>(
+    undefined,
+  );
+  const [selectedSection, setSelectedSection] = useState<string | undefined>(
+    undefined,
+  );
+  const [keyword, setKeyword] = useState<string>("");
+  const router = useRouter();
+
+  const toggleAccordion = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  useEffect(() => {
+    fetchData(page, rowsPerPage);
+  }, [page, rowsPerPage]);
+  console.log(data);
+  const notices = [
+    {
+      id: 1,
+      title: "Independence day celebration",
+      publishDate: "13-08-2024",
+      noticeDate: "12-08-2024",
+      createdBy: "Super Admin",
+      messageTo: "Student",
+    },
+    {
+      id: 2,
+      title: "Independence day celebration",
+      publishDate: "13-08-2024",
+      noticeDate: "12-08-2024",
+      createdBy: "Super Admin",
+      messageTo: "Student",
+    },
+  ];
+
+  const handleDelete = async (id: number) => {
+    try {
+      const response = await deleteNotificationData(id);
+
+      if (response.status == 200) {
+        toast.success("Delete successful");
+      } else {
+        toast.error("Error Delete ");
+      }
+      fetchData(page, rowsPerPage);
+    } catch (error) {
+      console.error("Delete failed", error);
+    }
+  };
+
+  const handleEdit = (id: number) => {
+    router.push(`/admin/notic_board/edit/${id}`);
+  };
+
+  const fetchData = async (currentPage: number, rowsPerPage: number) => {
+    try {
+      const result = await fetchNotificationData(currentPage + 1, rowsPerPage);
+      setTotalCount(result.totalCount);
+      setData(result.data);
+      setLoading(false);
+    } catch (error: any) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+  return (
+    <DefaultLayout>
+      <section className="content py-6">
+        <div className="mx-auto max-w-7xl">
+          <div className="rounded-lg bg-white shadow-lg">
+            <div className="flex items-center justify-between bg-[#4a5f87] p-4 text-white">
+              <h3 className="flex items-center text-lg font-bold">
+                <i className="fa fa-commenting-o mr-2" />
+                Notice Board
+              </h3>
+              <Link href="/admin/notic_board/add">
+                <button className="btn btn-primary rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600">
+                  <i className="fa fa-plus mr-2" />
+                  Post New Message
+                </button>
+              </Link>
+            </div>
+
+            <div className="p-4 dark:bg-boxdark dark:drop-shadow-none">
+              {data.map((notice: any, index) => (
+                <div
+                  key={notice.id}
+                  className="mb-4 rounded-lg border border-stroke bg-transparent text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark"
+                >
+                  <div className="bg-gray-200 flex items-center justify-between p-3 dark:border-strokedark dark:bg-boxdark dark:text-white dark:drop-shadow-none">
+                    <div
+                      className="cursor-pointer"
+                      onClick={() => toggleAccordion(index)}
+                    >
+                      <h4 className="font-medium text-black dark:text-white ">
+                        {notice.title}
+                      </h4>
+                    </div>
+
+                    <div key={notice.id}>
+                      <IconButton
+                        className="dark:text-white"
+                        onClick={() => handleEdit(notice.id)}
+                        aria-label="Edit"
+                      >
+                        <Edit />
+                      </IconButton>
+                      <IconButton
+                        className="dark:text-white"
+                        onClick={() => handleDelete(notice.id)}
+                        aria-label="delete"
+                      >
+                        <Delete />
+                      </IconButton>
+                    </div>
+                  </div>
+
+                  {activeIndex === index && (
+                    <div className="border-t border-stroke p-4 dark:border-strokedark ">
+                      <div className="grid grid-cols-1 gap-4 dark:border-strokedark dark:bg-boxdark dark:text-white dark:drop-shadow-none md:grid-cols-2">
+                        {/* Left Column */}
+                        <div>
+                          <p>{notice.title}</p>
+                          {notice.path ? (
+                            <Image
+                              src={
+                                process.env.NEXT_PUBLIC_BASE_URL + notice.path
+                              }
+                              className="h-28 w-28 rounded-lg object-cover"
+                              alt="No Image"
+                            />
+                          ) : (
+                            ""
+                          )}
+                        </div>
+
+                        {/* Right Column */}
+                        <div className="col-span-1">
+                          <div className="rounded border border-stroke p-4 dark:border-strokedark">
+                            <ul className="space-y-2">
+                              <li>
+                                <i className="fa fa-calendar-check-o mr-2 dark:text-white" />
+                                Publish Date: {notice.publish_date}
+                              </li>
+                              <li>
+                                <i className="fa fa-calendar mr-2 dark:text-white" />
+                                Notice Date: {notice.date}
+                              </li>
+                              <li>
+                                <i className="fa fa-user mr-2 dark:text-white" />
+                                Created By: {notice.created_by}
+                              </li>
+                            </ul>
+                            <h4 className="mt-4 text-blue-500 dark:text-white">
+                              Message To
+                            </h4>
+                            <ul className="space-y-2 dark:text-white">
+                              <li>
+                                <i className="fa fa-user mr-2 dark:text-white" />
+                                {notice.id}
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+};
+
+export default StudentDetails;
