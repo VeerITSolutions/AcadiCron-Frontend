e1322c23c9abce71ee0386761a1bb9c3abd572a7
pranay-lamse <pranay.l@veerit.com>
2025-06-25 13:30:19 +0530

    added

---
+++ src/app/admin/student_details_search/page.tsx
@@ -0,0 +1,333 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import MUIDataTable from "mui-datatables";
+import { useGlobalState } from "@/context/GlobalContext";
+import { fetchStudentData } from "@/services/studentService";
+import styles from "./StudentDetails.module.css"; // Import CSS module
+import Loader from "@/components/common/Loader";
+import {
+  fetchsectionByClassData,
+  fetchsectionData,
+} from "@/services/sectionsService"; // Import your section API service
+import { getClasses } from "@/services/classesService"; // Import your classes API service
+
+import { ThemeProvider } from "@mui/material/styles";
+import useColorMode from "@/hooks/useColorMode";
+import { darkTheme, lightTheme } from "@/components/theme/theme";
+import { useSearchParams } from "next/navigation";
+import {
+  Edit,
+  Delete,
+  Visibility,
+  TextFields,
+  AttachMoney,
+} from "@mui/icons-material";
+import IconButton from "@mui/material/IconButton";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+  TextField,
+} from "@mui/material";
+import { toast } from "react-toastify";
+import { useLoginDetails } from "@/store/logoStore";
+
+const columns = [
+  "Admission No",
+  "Student Name",
+  "Class",
+  "Category",
+  "Mobile Number",
+  "Action",
+];
+
+const options = {
+  filterType: "checkbox",
+  serverSide: true,
+  responsive: "standard",
+  search: false,
+  selectableRows: "none", // Disable row selection
+
+  filter: false, // Disable filter,
+  viewColumns: false, // Disable view columns button
+};
+
+const StudentDetails = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [colorMode, setColorMode] = useColorMode();
+  const [data, setData] = useState<Array<Array<string>>>([]);
+  const { themType, setThemType } = useGlobalState(); //
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [totalCount, setTotalCount] = useState(0);
+  const [classes, setClassessData] = useState<Array<any>>([]);
+  const [section, setSections] = useState<Array<any>>([]);
+  const [selectedClass, setSelectedClass] = useState<string | undefined>();
+  const [selectedSection, setSelectedSection] = useState<string | undefined>(
+    undefined,
+  );
+  const initialKeyword = searchParams?.get("s") || "";
+  const [keyword, setKeyword] = useState<string>(initialKeyword);
+
+  const formatStudentData = (students: any[]) => {
+    return students.map((student: any) => [
+      student.admission_no || "N/A",
+
+      `${student.firstname.trim()} ${student.lastname.trim()}`,
+      `${student.class_name} (${student?.section_name})` || "N/A",
+      student.category_name || "N/A",
+      student.mobileno || "N/A",
+      <div key={student.id} className="flex text-left">
+        <IconButton onClick={() => handleView(student.id)} aria-label="Show">
+          <Visibility />
+        </IconButton>
+        <IconButton onClick={() => handleEdit(student.id)} aria-label="Edit">
+          <Edit />
+        </IconButton>
+        <IconButton
+          onClick={() => handleAddFees(student.id)}
+          aria-label="Add Fee"
+        >
+          <AttachMoney />
+        </IconButton>
+      </div>,
+    ]);
+  };
+  const [selectedSessionId, setSelectedSessionId] = useState<string | null>(
+    null,
+  );
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const searchKeyword = searchParams.get("s");
+    if (searchKeyword) {
+      setKeyword(searchKeyword);
+    }
+  }, []);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const searchKeyword = searchParams.get("s");
+    if (searchKeyword) {
+      setKeyword(searchKeyword);
+      fetchData(
+        page,
+        rowsPerPage,
+        selectedClass,
+        selectedSection,
+        searchKeyword,
+      );
+    }
+  }, []);
+  const getselectedSessionId = useLoginDetails(
+    (state) => state.selectedSessionId,
+  );
+  useEffect(() => {
+    setSelectedSessionId(getselectedSessionId);
+  }, []);
+  const fetchData = async (
+    currentPage: number,
+    rowsPerPage: number,
+    selectedClass?: string,
+    selectedSection?: string,
+    keyword?: string,
+  ) => {
+    try {
+      // Pass selectedClass and selectedSection as parameters to filter data
+
+      if (selectedClass) {
+        const result = await fetchStudentData(
+          currentPage + 1,
+          rowsPerPage,
+          selectedClass,
+          selectedSection,
+          keyword,
+          selectedSessionId,
+        );
+        setTotalCount(result.totalCount);
+        const formattedData = formatStudentData(result.data);
+        setData(formattedData);
+        setLoading(false);
+      }
+
+      if (keyword && keyword.trim() !== "" && !selectedClass) {
+        const result = await fetchStudentData(
+          currentPage + 1,
+          rowsPerPage,
+          selectedClass,
+          selectedSection,
+          keyword,
+          selectedSessionId,
+        );
+        setTotalCount(result.totalCount);
+        const formattedData = formatStudentData(result.data);
+        setData(formattedData);
+        setLoading(false);
+      }
+      setLoading(false);
+    } catch (error: any) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  const fetchClassesAndSections = async () => {
+    try {
+      const classesResult = await getClasses();
+      setClassessData(classesResult.data);
+
+      // Fetch sections if a class is selected
+      if (selectedClass) {
+        const sectionsResult = await fetchsectionByClassData(selectedClass);
+        setSections(sectionsResult.data);
+      } else {
+        setSections([]); // Clear sections if no class is selected
+      }
+    } catch (error: any) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  const handleView = async (id: number) => {
+    // Assuming id is the student_id
+    router.push(`/admin/student/${id}`);
+  };
+
+  const handleEdit = (id: number) => {
+    router.push(`/admin/student/edit/${id}`);
+  };
+
+  const handleAddFees = (id: number) => {
+    router.push(`/admin/student/fees/${id}`);
+  };
+
+  useEffect(() => {
+    fetchClassesAndSections(); // Fetch classes and sections on initial render
+  }, [selectedClass]);
+
+  useEffect(() => {
+    fetchData(page, rowsPerPage, selectedClass, selectedSection, keyword);
+  }, [page, rowsPerPage, selectedClass, selectedSection, keyword]);
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleRowsPerPageChange = (newRowsPerPage: number) => {
+    setRowsPerPage(newRowsPerPage);
+    setPage(0);
+  };
+
+  const handleClassChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedClass(event.target.value);
+    setPage(0);
+  };
+
+  const handleSectionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedSection(event.target.value);
+    setPage(0);
+  };
+
+  const handleKeywordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+
+  const handleSearch = () => {
+    setPage(0); // Reset to first page on search
+    fetchData(page, rowsPerPage, selectedClass, selectedSection, keyword);
+  };
+  const handleRefresh = () => {
+    setSelectedClass("");
+    setSelectedSection("");
+    setData([]);
+    setKeyword("");
+  };
+
+  /* if (loading) return <Loader />; */
+  if (error) return <p>{error}</p>;
+
+  return (
+    <DefaultLayout>
+      <div className={styles.filters}>
+        <div className={styles.filterGroup}>
+          <label className={styles.label}>
+            Class:
+            <select
+              value={selectedClass || ""}
+              onChange={handleClassChange}
+              className={`${styles.select} rounded-lg border-stroke outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary`}
+            >
+              <option value="">Select</option>
+              {classes.map((cls) => (
+                <option key={cls.id} value={cls.id}>
+                  {cls.class}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label className={styles.label}>
+            Section:
+            <select
+              value={selectedSection || ""}
+              onChange={handleSectionChange}
+              className={`${styles.select} rounded-lg border-stroke outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary`}
+              disabled={!selectedClass} // Disable section dropdown if no class is selected
+            >
+              <option value="">Select</option>
+              {section.map((sec) => (
+                <option key={sec.section_id} value={sec.section_id}>
+                  {sec.section_name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <div className={styles.searchGroup}>
+            <input
+              type="text"
+              placeholder="Search By Keyword"
+              value={keyword}
+              onChange={handleKeywordChange}
+              className={`${styles.searchInput} rounded-lg border-stroke outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary`}
+            />
+            <button onClick={handleSearch} className={styles.searchButton}>
+              Search
+            </button>
+            <button onClick={handleRefresh} className={styles.searchButton}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {loading ? (
+        <Loader />
+      ) : (
+        <ThemeProvider theme={themType === "dark" ? darkTheme : lightTheme}>
+          <MUIDataTable
+            title={"Student Details"}
+            data={data}
+            columns={columns}
+            options={{
+              ...options,
+              count: totalCount,
+              page: page,
+              rowsPerPage: rowsPerPage,
+              onChangePage: handlePageChange,
+              onChangeRowsPerPage: handleRowsPerPageChange,
+            }}
+          />
+        </ThemeProvider>
+      )}
+    </DefaultLayout>
+  );
+};
+
+export default StudentDetails;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -67,7 +67,7 @@ const Header = (props: {
         </div>
 
         <div className="hidden sm:block">
-          <form action="/admin/student_details" method="GET">
+          <form action="/admin/student_details_search" method="GET">
             <div className="relative">
               <button className="absolute left-0 top-1/2 -translate-y-1/2">
                 <svg
